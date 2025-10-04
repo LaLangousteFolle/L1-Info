ef1cf0399623a979d7d0a3ffe777a89c0270d598
LaLangousteFolle <seilerlebed@gmail.com>
2025-10-04 12:01:06 +0200

    Fix Mastermind score calculation and improve code quality
    The changes improve the Mastermind game implementation with:
    - More accurate scoring algorithm that handles duplic

--- TP3/Mastermind.py
+++ TP3/Mastermind.py
@@ -1,53 +1,65 @@
 from random import *
+
+
 def Init(n, k):
-	L = []
-	for i in range (n):
-		L.append(randint(1, k))
-	return(L)
+    L = []
+    for i in range(n):
+        L.append(randint(1, k))
+    return L
+
 
 def saisir_proposition():
-	L1=list(input("Entrez une liste de chiffres\n"))
-	i = 0
-	H = []
-	for i in range(len(L1)):
-		if L1[i] >= '0' and L1[i] <= '9':
-			H.append(int(L1[i]))
-	return(H)
+    L1 = list(input("Entrez une liste de chiffres\n"))
+    i = 0
+    H = []
+    for i in range(len(L1)):
+        if L1[i] >= "0" and L1[i] <= "9":
+            H.append(int(L1[i]))
+    return H
 
-def Score(H,R):
-	juste = 0
-	mal_place = 0
-	found = False
-	if len(H) == len(R):
-		for i in range(len(R)):
-			for j in range(len(R)):
-				if H[i] == R[i]:
-					juste += 1
-					break
-				elif H[j] == R[i]:
-					mal_place += 1
-					break
-	return(juste, mal_place)			
+
+def Score(H, R):
+    if len(H) != len(R):
+        exit("Length diff")
+    juste = 0
+    mal_place = 0
+    H_used = [False] * len(H)
+    R_used = [False] * len(R)
+    for i in range(len(H)):
+        if H[i] == R[i]:
+            juste += 1
+            H_used[i] = True
+            R_used[i] = True
+    for i in range(len(H)):
+        if not H_used[i]:
+            for j in range(len(R)):
+                if H[i] == R[j] and not R_used[j]:
+                    mal_place += 1
+                    R_used[j] = True
+                    break
+    return (juste, mal_place)
+
 
 def jouer():
-	essais = 10
-	n = int(input("Choisissez la taille du la combinaison a trouver\n"))
-	k = int(input("Choisissez la valeur maximal des chiffres du jeu (de 1 a 9 max)\n"))
-	R = Init(n,k)
-	while essais > 0:
-		juste, mal_place = Score(saisir_proposition(), R) 
-		print(f"{juste} sont bien placé et {mal_place} mal placés")
-		if juste == n:
-			print("Vous avez gagné")
-			essais = 0
-		essais -= 1
-			
-def main():
-	play= bool(int(input("Ecrire 1 si vous souhaitez jouer, sinon 0\n")))
-	while play:
-		jouer()
-		play = bool(int(input("Tappez 1 si vous souhaitez rejouer, sinon 0\n")))
-	
-if __name__ == '__main__':
-	main()
+    essais = 10
+    n = int(input("Choisissez la taille du la combinaison a trouver\n"))
+    k = int(input("Choisissez la valeur maximal des chiffres du jeu (de 1 a 9 max)\n"))
+    R = Init(n, k)
+    while essais > 0:
+        juste, mal_place = Score(saisir_proposition(), R)
+        print(f"{juste} sont bien placé et {mal_place} mal placés")
+        if juste == n:
+            print("Vous avez gagné")
+            essais = 0
+        essais -= 1
 
+
+def main():
+    play = bool(int(input("Ecrire 1 si vous souhaitez jouer, sinon 0\n")))
+    while play:
+        jouer()
+        play = bool(int(input("Tappez 1 si vous souhaitez rejouer, sinon 0\n")))
+
+
+if __name__ == "__main__":
+    main()
--- TP3/Mastermind2.py
+++ TP3/Mastermind2.py
@@ -16,17 +16,23 @@ def toutes_possibilites(n, longueur):
 
 
 def Score(H, R):
+    if len(H) != len(R):
+        exit("Length diff")
     juste = 0
     mal_place = 0
-    found = False
-    if len(H) == len(R):
-        for i in range(len(R)):
+    H_used = [False] * len(H)
+    R_used = [False] * len(R)
+    for i in range(len(H)):
+        if H[i] == R[i]:
+            juste += 1
+            H_used[i] = True
+            R_used[i] = True
+    for i in range(len(H)):
+        if not H_used[i]:
             for j in range(len(R)):
-                if H[i] == R[i]:
-                    juste += 1
-                    break
-                elif H[j] == R[i]:
+                if H[i] == R[j] and not R_used[j]:
                     mal_place += 1
+                    R_used[j] = True
                     break
     return (juste, mal_place)
 
@@ -34,22 +40,35 @@ def Score(H, R):
 def devine(longeur, n):
     essais = 0
     H = toutes_possibilites(n, longeur)
+    H1 = []
     Ens_H = []
     Scores_H = []
     found = False
-    Ens_H.append(H[randint(0, len(H - 1))])
+    temp = H[randint(0, len(H) - 1)]
+
+    print(temp)
+
     while not (found):
-        temp = Ens_H[randint(0, len(H - 1))]
-        print(temp)
         Ens_H.append(temp)
         essais += 1
-        juste = int(input("Combien sont bien placés?"))
-        mp = int(input("Combien sont mal placés?"))
+        juste = int(input("Combien sont bien placés?\n"))
+        mp = int(input("Combien sont mal placés?\n"))
         Scores_H.append([juste, mp])
+
         if juste == longeur:
             found = True
-            print("Trouve")
+            return f"Trouve en {essais} essais"
+
         for i in range(len(H)):
-            bon, pb = Score(temp, H[i])
-            if bon == juste and pb == mp:
-                Ens_H.append(H[i])
+            for j in range(len(Ens_H)):
+                bon, pb = Score(H[i], Ens_H[j])
+
+                if bon == Scores_H[j][0] and pb == Scores_H[j][1]:
+                    H1.append(H[i])
+        H = H1
+        temp = H[randint(0, len(H) - 1)]
+        Ens_H.append(temp)
+        print(temp)
+
+
+print(devine(2, 3))
