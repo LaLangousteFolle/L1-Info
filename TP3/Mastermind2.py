bca2e865f72811d7ee0f1dbeec73536a31ea4b28
LaLangousteFolle <seilerlebed@gmail.com>
2025-10-04 10:16:33 +0200

    Format code style and fix syntax errors
    The changes focus on improving code formatting through consistent
    indentation, whitespace, and parentheses. Also fixes syntax errors in
    function calls and parameters.

--- TP3/Mastermind2.py
+++ TP3/Mastermind2.py
@@ -1,53 +1,55 @@
-from random import *
-def toutes_possibilites(n, longueur):
-	possibilites = [[]]
-	for _ in range(longueur):
-		nouvelles = []
-		for c in range(len(possibilites)):
-			comb= possibilites[c]
-				for i in range(1, n+1):
-					L=list(comb)
-					L.append(i)
-					nouvelles.append(L)
-			possibilites = nouvelles
-	return possibilites
+from random import randint
 
-def Score(H,R):
-	juste = 0
-	mal_place = 0
-	found = False
-	if len(H) == len(R):
-		for i in range(len(R)):
-			for j in range(len(R)):
-				if H[i] == R[i]:
-					juste += 1
-					break
-				elif H[j] == R[i]:
-					mal_place += 1
-					break
-	return(juste, mal_place)			
+
+def toutes_possibilites(n, longueur):
+    possibilites = [[]]
+    for _ in range(longueur):
+        nouvelles = []
+        for c in range(len(possibilites)):
+            comb = possibilites[c]
+            for i in range(1, n + 1):
+                L = list(comb)
+                L.append(i)
+                nouvelles.append(L)
+        possibilites = nouvelles
+    return possibilites
+
+
+def Score(H, R):
+    juste = 0
+    mal_place = 0
+    found = False
+    if len(H) == len(R):
+        for i in range(len(R)):
+            for j in range(len(R)):
+                if H[i] == R[i]:
+                    juste += 1
+                    break
+                elif H[j] == R[i]:
+                    mal_place += 1
+                    break
+    return (juste, mal_place)
 
 
 def devine(longeur, n):
-	essais = 0 
-	H = toutes_possibilites(n, longeur)
-	Ens_H = []
-	Scores_H =[]
-	found = False
-	Ens_H.append(H[randint(0, len(H-1))])
-	while not(found):
-		temp =Ens_H[randint(0, len(H-1))])
-		print(temp)
-		Ens_H.append(temp)
-		essais += 1
-		juste = int(input("Combien sont bien placés?")
-		mp = int(input("Combien sont mal placés?")
-		Scores_H.append([juste, mp])
-		if juste == longeur:
-			found = True
-			print("Trouve")
-		for i in range(len(H)):
-			bon, pb= Score(temp,H[i])
-			if bon == juste and pb == mp:
-				Ens_H.append(H[i])
-				 
+    essais = 0
+    H = toutes_possibilites(n, longeur)
+    Ens_H = []
+    Scores_H = []
+    found = False
+    Ens_H.append(H[randint(0, len(H - 1))])
+    while not (found):
+        temp = Ens_H[randint(0, len(H - 1))]
+        print(temp)
+        Ens_H.append(temp)
+        essais += 1
+        juste = int(input("Combien sont bien placés?"))
+        mp = int(input("Combien sont mal placés?"))
+        Scores_H.append([juste, mp])
+        if juste == longeur:
+            found = True
+            print("Trouve")
+        for i in range(len(H)):
+            bon, pb = Score(temp, H[i])
+            if bon == juste and pb == mp:
+                Ens_H.append(H[i])
