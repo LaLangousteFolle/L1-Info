74cca439476652048f4b7bb4068cd12073048872
Noe SEILER <seilerlebed@gmail.com>
2025-10-27 10:06:44 +0100

    Deleted useless compilation file and added sum exercises to the TP1 :3

--- C/TP1/ex1-1.c
+++ C/TP1/ex1-1.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 /*
 Exo1.1
 int   main(void)
 {
   int a,b,c;
   
-  __builtin_printf("a = %i\n", a);
-  __builtin_printf("b = %i\n", b);
-  __builtin_printf("c = %i\n", c);
+  printf("a = %i\n", a);
+  printf("b = %i\n", b);
+  printf("c = %i\n", c);
 
 }
 */
@@ -21,8 +22,40 @@ int   main(void)
   b = 5;
   c = a + b;
 
-  __builtin_printf("a = %i\n", a);
-  __builtin_printf("b = %i\n", b);
-  __builtin_printf("c = %i\n", c);
+  printf("a = %i\n", a);
+  printf("b = %i\n", b);
+  printf("c = %i\n", c);
+}
+*/
+
+/*
+Exo1.3
+int   main(void)
+{
+  float x,y;
+
+  x = 7.5;
+  y = 3.2;
+  printf("Somme = %d\n", x + y);
+  printf("Soustraction = %d\n", x - y);
+  printf("Multiplication = %d\n", x * y);
+  printf("Division = %d\n", x / y);
+}
+*/
+
+/*
+Exo1.4
+int   main(void)
+{
+  float x, y;
+
+  scanf("Entrez une valeur de x : %d\n", &x);
+  scanf("Entrez une valeur de y : %d\n", &y);
+
+  printf("Somme = %d\n", x + y);
+  printf("Soustraction = %d\n", x - y);
+  printf("Multiplication = %d\n", x * y);
+  printf("Division = %d\n", x / y);
+
 }
 */
