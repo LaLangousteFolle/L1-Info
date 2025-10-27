1e45a5cfcfdde8a8b137b9f109253b776446e955
Noe SEILER <seilerlebed@gmail.com>
2025-10-27 09:51:30 +0100

    Reworked files, and added the first C TP as I was sick :/

---
+++ C/TP1/ex1-1.c
@@ -0,0 +1,28 @@
+/*
+Exo1.1
+int   main(void)
+{
+  int a,b,c;
+  
+  __builtin_printf("a = %i\n", a);
+  __builtin_printf("b = %i\n", b);
+  __builtin_printf("c = %i\n", c);
+
+}
+*/
+
+/*
+Exo1.2
+int   main(void)
+{
+  int a,b,c;
+
+  a = 10;
+  b = 5;
+  c = a + b;
+
+  __builtin_printf("a = %i\n", a);
+  __builtin_printf("b = %i\n", b);
+  __builtin_printf("c = %i\n", c);
+}
+*/
