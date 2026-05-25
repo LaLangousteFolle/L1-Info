63584993af5d71a7d117e167bc332f621e9f76c2
nono <seilerlebed@gmail.com>
2026-05-25 21:16:37 +0200

    added TP7
    Signed-off-by: nono <seilerlebed@gmail.com>

--- Dev web/Mini-projet/index.php
+++ Dev web/Mini-projet/index.php
@@ -1,8 +1,11 @@
 <html>
     <head>
-
+        <h1>Notes culinaires</h1>
     </head>
     <body>
 
     </body>
+    <footer>
+        <p>Date: Mai 2026 <br> Auteur: Noe SEILER</p>
+    </footer>
 </html>
